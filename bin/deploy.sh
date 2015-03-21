f1cbce7c3d522c60e07060f5b05501faa87f640f
Colby Rabideau <crabideau@hubspot.com>
2015-03-21 12:34:10 -0400

    remove deploy.sh

--- bin/deploy.sh
+++
@@ -1,6 +0,0 @@
-npm test &&
-  flow check &&
-  npm run build-hintable &&
-  git add hintable &&
-  git commit -m "build hintable for \"$(git --no-pager log -n 1 --pretty=oneline)\"" hintable/;
-git push
