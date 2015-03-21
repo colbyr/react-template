a9cf1018dd8a41165c26e70b7a3e2533e04008ee
Colby Rabideau <crabideau@hubspot.com>
2015-03-21 13:17:07 -0400

    basic react router setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-var Dispatcher = require('flux').Dispatcher;
-var GeneralStore = require('general-store');
-var React = require('react/addons');
+import { Dispatcher } from 'flux';
+import { DispatcherInstance } from 'general-store';
+import React from 'react/addons';
+import ReactRouter from 'react-router';
+import Routes from './Routes';
 
 var dispatcher = new Dispatcher();
 var rootNode = document.getElementById('root');
 
-GeneralStore.DispatcherInstance.set(dispatcher);
+DispatcherInstance.set(dispatcher);
 
-React.render(
-  <h1>react-template</h1>,
-  rootNode
-);
+ReactRouter.run(Routes, Handler => {
+  React.render(<Handler />, rootNode);
+});
 
 module.exports = {
-  dispatcher,
-  rootNode
+  dispatcher: dispatcher,
+  rootNode: rootNode
 };
 
